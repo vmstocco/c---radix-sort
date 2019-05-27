ead30defd5d2eea7213141c26e4a3fba466a2eb0
vmstocco <vmstocco@gmail.com>
2019-05-27 22:57:18 +0100

    Update radixsort.c

--- radixsort.c
+++ radixsort.c
@@ -1,7 +1,7 @@
 /*
                              Trabalho 3 de ICC 2 - Radix Sort
 
-                    Vinícius Marques Stocco - nº USP: 6427233 - Professora: Rosane Minghim
+                    VinÃ­cius Marques Stocco - Professora: Rosane Minghim
 */
 
 #include <stdio.h>
@@ -14,8 +14,8 @@ typedef struct {
 } registro;
 
 typedef struct {
-    int n;           // número de registros num vetor auxiliar;
-    registro *lista; // lista é o vetor com o número "n" de registros;
+    int n;           // nÃºmero de registros num vetor auxiliar;
+    registro *lista; // lista Ã© o vetor com o nÃºmero "n" de registros;
 } registros_aux;
 
 void inicializar(int fn, registros_aux vet_aux[10]) {
@@ -24,10 +24,10 @@ void inicializar(int fn, registros_aux vet_aux[10]) {
     // inicializa o tamanho dos vetores auxiliares com zero;
     for (i = 0; i < 10; i++) vet_aux[i].n = 0;
 
-    // lê quantos registros serão inseridos;
+    // lÃª quantos registros serÃ£o inseridos;
     //scanf ("%d", &fn);
 
-    // aloca a quantidade máxima que um vetor auxiliar pode armazenar, que é igual à "fn";
+    // aloca a quantidade mÃ¡xima que um vetor auxiliar pode armazenar, que Ã© igual Ã  "fn";
     for (i = 0; i < 10; i++) vet_aux[i].lista = (registro *)malloc(fn*sizeof(registro));
 };
 
@@ -44,11 +44,11 @@ void entrada(const int fn, registro vetreg_aux[]) {
 
 void ordenar(const int fn, registro vetreg_aux[], registros_aux vet_aux[10]) {
     int i,k;
-    int l = 0; // representa as posições no vetor original;
-    int j = 6; // representa a posição dos dígitos;
-    int pos;   // um inteiro do número usp;
-    int tam;   // número de registros num vetor auxiliar;
-    char c;    // um caracter do número usp, que será convertido para int (pos);
+    int l = 0; // representa as posiÃ§Ãµes no vetor original;
+    int j = 6; // representa a posiÃ§Ã£o dos dÃ­gitos;
+    int pos;   // um inteiro do nÃºmero usp;
+    int tam;   // nÃºmero de registros num vetor auxiliar;
+    char c;    // um caracter do nÃºmero usp, que serÃ¡ convertido para int (pos);
 
     while (j != -1) {
         // percorre todos os registros selecionando um caracter e inserindo num vetor auxiliar, de acordo com o valor do caracter;
@@ -64,7 +64,7 @@ void ordenar(const int fn, registro vetreg_aux[], registros_aux vet_aux[10]) {
         for (i = 0; i < 10; i++) {
             // verifica se tem algum elemento num vetor auxiliar;
             if (vet_aux[i].n > 0) {
-                // copia pro vetor original(vetreg_aux), na posição "l", os elementos que estão no vetor auxiliar;
+                // copia pro vetor original(vetreg_aux), na posiÃ§Ã£o "l", os elementos que estÃ£o no vetor auxiliar;
                 for (k = 0; k < vet_aux[i].n; k++, l++) {
                     vetreg_aux[l] = vet_aux[i].lista[k];
                 }
@@ -72,7 +72,7 @@ void ordenar(const int fn, registro vetreg_aux[], registros_aux vet_aux[10]) {
             }
         }
         l = 0;
-        j--;  // decrementa o dígito que está sendo analisado;
+        j--;  // decrementa o dÃ­gito que estÃ¡ sendo analisado;
     }
 };
 
@@ -81,17 +81,17 @@ int main () {
     registros_aux vetores_auxiliares[10]; // vetores auxiliares;
     registro *vetreg;                     // vetor original;
     int j;
-    int n;                                // número de registros;
+    int n;                                // nÃºmero de registros;
 
-    // lê quantos registros serão inseridos(n); inicializa os vetores_auxiliares[i].n com zero; aloca "n" posições em cada vetor auxiliar;
+    // lÃª quantos registros serÃ£o inseridos(n); inicializa os vetores_auxiliares[i].n com zero; aloca "n" posiÃ§Ãµes em cada vetor auxiliar;
     printf("Type the number of inputs:");
     scanf ("%d", &n);
     inicializar(n,vetores_auxiliares);
 
-    // vetor original(vetreg), onde são inseridos os registros;
+    // vetor original(vetreg), onde sÃ£o inseridos os registros;
     vetreg = (registro *)malloc(n*sizeof(registro));
 
-    //lê o número usp e o nome;
+    //lÃª o nÃºmero usp e o nome;
     entrada(n,vetreg);
 
     // ordena os registros;
@@ -100,7 +100,7 @@ int main () {
     // exibi os registros ordenados;
     for (j = 0; j < n; j++) printf ("%s %s\n", vetreg[j].nro_usp, vetreg[j].nome);
 
-    // libera a memória alocada;
+    // libera a memÃ³ria alocada;
     for (j = 0; j < 10; j++) free(vetores_auxiliares[j].lista);
 
     return 0;
